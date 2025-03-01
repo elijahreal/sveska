99ee5922b14c74164d432abbefc63c4ef12075bd
elijahreal <elijahreal00@gmail.com>
2025-03-02 00:08:47 +0100

    Initial Commit

---
+++ sveska/sveskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sveskaApp.swift
+//  sveska
+//
+//  Created by Ilija Djurkovic on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct sveskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
